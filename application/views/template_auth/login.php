5db494e4113d7dc8513fc9d16ac9b3b239a56809
henry <henry>
2024-05-27 11:24:46 +0700

    update login screen

--- application/views/template_auth/login.php
+++ application/views/template_auth/login.php
@@ -21,7 +21,12 @@
 		background-color: white;
 	}
 
-	.form-control, .select2-container--default .select2-selection--single, .select2-container--default .select2-selection--single .select2-search__field, .typeahead, .tt-query, .tt-hint {
+	.form-control,
+	.select2-container--default .select2-selection--single,
+	.select2-container--default .select2-selection--single .select2-search__field,
+	.typeahead,
+	.tt-query,
+	.tt-hint {
 		border: 1px solid #c3dafb;
 	}
 
@@ -33,8 +38,9 @@
 	/* Small devices (landscape phones, 576px and up) */
 	@media (min-width: 400px) {
 		.form-login {
-			margin-top:10px;
+			margin-top: 10px;
 		}
+
 		.txt-login {
 			text-align: center;
 		}
@@ -42,54 +48,64 @@
 		.brand-logo {
 			text-align: center;
 		}
+
 		.brand-logo img {
-			width:10%;
+			width: 10%;
 		}
-	 }
+	}
 
 	/* Medium devices (tablets, 768px and up) */
 	@media (min-width: 768px) {
 		.form-login {
-			margin-top:100px;
+			margin-top: 100px;
 		}
+
 		.txt-login {
 			text-align: left;
 		}
+
 		.brand-logo img {
-			width:15%;
+			width: 15%;
 		}
+
 		.brand-logo {
 			text-align: left;
 		}
 	}
 
 	/* Large devices (desktops, 992px and up) */
-	@media (min-width: 992px) { 
+	@media (min-width: 992px) {
 		.form-login {
-			margin-top:100px;
+			margin-top: 100px;
 		}
+
 		.txt-login {
 			text-align: left;
 		}
+
 		.brand-logo img {
-			width:35%;
+			width: 35%;
 		}
+
 		.brand-logo {
 			text-align: left;
 		}
 	}
 
 	/* Extra large devices (large desktops, 1200px and up) */
-	@media (min-width: 1200px) { 
+	@media (min-width: 1200px) {
 		.form-login {
-			margin-top:170px;
+			margin-top: 170px;
 		}
+
 		.txt-login {
 			text-align: left;
 		}
+
 		.brand-logo img {
-			width:35%;
+			width: 35%;
 		}
+
 		.brand-logo {
 			text-align: left;
 		}
@@ -99,53 +115,54 @@
 	<div class="content-wrapper d-flex align-items-center auth p-0">
 		<div class="row h-100 p-0">
 			<div class="col-md-6 p-0">
-			<div class="col-lg-8 p-0 mx-auto form-login" style="">
-				<div class="auth-form-light text-left py-3 px-4 px-sm-5">
-					<div class="brand-logo" style="margin-bottom: 10px;">
-						<img src="<?= base_url('assets/images/splash4.png'); ?>" alt="logo">
-					</div>
-					<!-- <h4 class="text-center">Masuk ke akun Anda</h4> -->
-					<h5 class="font-weight-light mb-4 txt-login">Masuk ke akun Anda.</h5>
-					<form class="pt-3" action="" method="post">
-						<?php if (validation_errors()) : ?>
-							<div class="alert alert-danger alert-dismissible fade show" role="alert">
-								<!-- <strong>Warning!</strong>
+				<div class="col-lg-8 p-0 mx-auto form-login" style="">
+					<div class="auth-form-light text-left py-3 px-4 px-sm-5">
+						<div class="brand-logo" style="margin-bottom: 10px;">
+							<img src="<?= base_url('assets/images/splash4.png'); ?>" alt="logo">
+						</div>
+						<!-- <h4 class="text-center">Masuk ke akun Anda</h4> -->
+						<h5 class="font-weight-light mb-4 txt-login">Masuk ke akun Anda.</h5>
+						<form class="pt-3" action="" method="post">
+							<?php if (validation_errors()) : ?>
+								<div class="alert alert-danger alert-dismissible fade show" role="alert">
+									<!-- <strong>Warning!</strong>
 								<hr> -->
-								<p><?= validation_errors(); ?></p>
-								<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-									<span aria-hidden="true">&times;</span>
-								</button>
+									<p><?= validation_errors(); ?></p>
+									<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+							<?php endif; ?>
+							<div class="form-group">
+								<input type="text" name="username" class="form-control form-control-sm focus" placeholder="Username">
 							</div>
-						<?php endif; ?>
-						<div class="form-group">
-							<input type="text" name="username" class="form-control form-control-sm focus" placeholder="Username">
-						</div>
-						<div class="form-group">
-							<input type="password" name="password" class="form-control form-control-sm" placeholder="password">
-						</div>
-						<div class="mt-3">
-							<button type="submit" class="btn btn-block btnlog btn-sm font-weight-medium auth-form-btn">Log in</button>
-						</div>
-						<div class="my-3 d-flex justify-content-center align-items-center">
-							<!-- <div class="form-check">
+							<div class="form-group">
+								<input type="password" name="password" class="form-control form-control-sm" placeholder="password">
+							</div>
+							<div class="mt-3">
+								<button type="submit" class="btn btn-block btnlog btn-sm font-weight-medium auth-form-btn">Log in</button>
+							</div>
+							<div class="my-3 d-flex justify-content-center align-items-center">
+								<!-- <div class="form-check">
 								<label class="form-check-label text-muted">
 									<input type="checkbox" class="form-check-input">
 									Keep me signed in
 								</label>
 							</div> -->
-							<a href="#" class="auth-link text-black">Forgot password?</a>
-						</div>
-						<!-- <div class="mb-2">
+								<a href="#" class="auth-link text-black">Forgot password?</a>
+							</div>
+							<!-- <div class="mb-2">
 							<button type="button" class="btn btn-block btn-facebook auth-form-btn">
 								<i class="typcn typcn-social-facebook-circular mr-2"></i>Connect using facebook
 							</button>
 						</div> -->
-					</form>
+						</form>
+					</div>
 				</div>
 			</div>
-			</div>
 			<div class="col-md-6 p-0">
-				<img src="<?= base_url('assets/images'); ?>/bottomimageweb-01.jpg" alt="" style="width:100%; height:100%; background-attachment: fixed; background-repeat: no-repeat;">
+				<img src="<?= base_url('assets/images'); ?>/login.png" alt="" style="width:100%; height:100%; background-attachment: fixed; background-repeat: no-repeat;">
+				<!-- <img src="<?= base_url('assets/images'); ?>/bottomimageweb-01.jpg" alt="" style="width:100%; height:100%; background-attachment: fixed; background-repeat: no-repeat;"> -->
 			</div>
 		</div>
 	</div>
