988b3130af6e5da6daa592ec619a45620039be03
Marfy Carlohanda Rasu <marfy@stardeptstore.com>
2024-09-09 17:14:22 +0700

    tambah column key discount

--- application/models/M_Supermarket.php
+++ application/models/M_Supermarket.php
@@ -55,7 +55,8 @@ class M_Supermarket extends CI_Model
             'DATE_FORMAT(th.trans_date, "%d-%M") AS "Date"',
             'DATE_FORMAT(th.trans_time, "%H:00") AS "Hour"',
             'CASE WHEN DAYNAME(th.trans_date) IN ("Sunday", "Saturday") THEN "WE" ELSE "WD" END AS "WD/WE"',
-            'CASE WHEN (th.trans_status = "3") THEN "TRADER" ELSE "NON TRADER" END AS Trader'
+            'CASE WHEN (th.trans_status = "3") THEN "TRADER" ELSE "NON TRADER" END AS Trader',
+            'CASE WHEN  left(td.promo_id,1) in ("B","S","") and (td.disc_pct <> 0 or td.moredisc_pct <> 0)  THEN "KEY DISCOUNT" ELSE "NON KEY DISCOUNT" end AS "Key Discount"'
         ]);
 
         $dbCentral->from('t_sales_trans_hdr th');
