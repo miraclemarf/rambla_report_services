baacab7c63e7c8ea377bc68ffc2b45da5c9dd1f5
Marfy Carlohanda Rasu <marfy@stardeptstore.com>
2024-09-09 11:47:12 +0700

    format file name specific date

--- application/controllers/ApiGenExcel.php
+++ application/controllers/ApiGenExcel.php
@@ -107,7 +107,7 @@ class ApiGenExcel extends CI_Controller
         $writer = new Xlsx($spreadsheet);
         $prefixFn = $store == '03' ? 'HH-sales-article_' : 'RSMKG-sales-article_';
         $dirName = $store == '03' ? '/HH' : '/RSMKG';
-        $filename = $prefixFn . date('d-m-Y', strtotime('-1 day'));
+        $filename = $prefixFn . date('d-m-Y', !$date ? strtotime('-1 day') : strtotime($date));
         $targetDir = rawurlencode('/STAR/RSHH'.$dirName);
         $accessToken = $this->M_TokenOneD->getAccessToken();
 
